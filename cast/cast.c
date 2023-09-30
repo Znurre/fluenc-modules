e99de7780b1ceefdb86ba3389a4858697b653881
Znurre <lova@avoidpointer.com>
2023-09-30 22:38:28 +0200

    Add Cast::toString

--- cast/cast.c
+++ cast/cast.c
@@ -1,4 +1,7 @@
-#include <stdint.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 
 int32_t toInt32(uint8_t value)
 {
@@ -8,4 +11,15 @@ int32_t toInt32(uint8_t value)
 int64_t toInt64(int32_t value)
 {
     return value;
+}
+
+void *toString(void* input)
+{
+    size_t size = *(size_t*)(input - sizeof(size_t));
+
+    char *output = calloc(size + 1, 1);
+
+    memcpy(output, input, size);
+
+    return output;
 }
