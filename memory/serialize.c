38bee4e0fc21b0c201a85a1ad6b95ccaf2c74dbd
Znurre <lova@avoidpointer.com>
2023-09-30 13:03:27 +0200

    Fix

--- memory/serialize.c
+++ memory/serialize.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <stdint.h>
 
-void set(void *buffer, size_t offset, int32_t value)
+int set(void *buffer, size_t offset, int32_t value)
 {
     printf("set: %lu = %i\n", offset, value);
 
     *(((char*)buffer) + offset) = (char)value;
 
-    return true;
+    return 1;
 }
